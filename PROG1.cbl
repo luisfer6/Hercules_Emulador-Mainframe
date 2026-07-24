3cd4e375560ce54a192a8ff4c6d3ace0cf626c33
luis_fernández_garcía <56836760+luisfer6@users.noreply.github.com>
2026-07-24 19:11:01 -0300

    Delete PROG1.cbl

--- PROG1.cbl
+++
@@ -1,6 +0,0 @@
-       IDENTIFICATION DIVISION.                                         
-       PROGRAM-ID. PROG1.                                               
-       PROCEDURE DIVISION.                                              
-      *89112                                                            
-           DISPLAY 'WELCOME TO CURSO1'.                                 
-           STOP RUN.                                                    
