60242168c9f8de1371d92b7d518f1cd6055da22a
luis_fernández_garcía <56836760+luisfer6@users.noreply.github.com>
2026-07-24 19:10:15 -0300

    Add files via upload

---
+++ CBLs/PROG1.cbl
@@ -0,0 +1,6 @@
+       IDENTIFICATION DIVISION.                                         
+       PROGRAM-ID. PROG1.                                               
+       PROCEDURE DIVISION.                                              
+      *89112                                                            
+           DISPLAY 'WELCOME TO CURSO1'.                                 
+           STOP RUN.                                                    
